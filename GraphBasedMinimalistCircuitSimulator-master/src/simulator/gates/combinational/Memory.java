fdaeb4d81bf27ad7433f9a6fe34f50bd65c56b2b
kowsar <kowsar.sh2000@gail.com>
2020-07-18 13:40:26 +0430

    Update Memory.java
    Now it's a 32 bit address memory with seprate read and write signals.

--- GraphBasedMinimalistCircuitSimulator-master/src/simulator/gates/combinational/Memory.java
+++ GraphBasedMinimalistCircuitSimulator-master/src/simulator/gates/combinational/Memory.java
@@ -2,13 +2,23 @@ package simulator.gates.combinational;
 
 import simulator.network.Link;
 import simulator.network.Node;
+/*
+Inputs:
+in[0]:read signal
+in[2]:Write signal
+in[2:33]:Address,needed for accesing memory
+in[34:65]:Data,Which is going to be written in memory at given address
 
+Outputs:
+out[0:31]:Data,which is read from memory
+
+*/
 public class Memory extends Node {
     private Boolean[] memory;
 
     public Memory(String label, Link... links) {
         super(label, links);
-        memory = new Boolean[65536];
+        memory = new Boolean[(int) (32*Math.pow(2,32))];
         for (int i = 0; i < 32; ++i) {
             addOutputLink(false);
         }
@@ -16,10 +26,10 @@ public class Memory extends Node {
 
     private int address() {
         int temp = 0;
-        for (int i = 1; i < 17; ++i) {
+        for (int i = 2; i < 34; ++i) {
             if(inputs.size() > i) {
                 if(getInput(i).getSignal()) {
-                    temp += Math.pow(2, 16 - i);
+                    temp += Math.pow(2, 33 - i);
                 }
             }
         }
@@ -27,8 +37,8 @@ public class Memory extends Node {
     }
 
     private void memoryWrite() {
-        for(int i = 17; i < 49; ++i) {
-            memory[address() + i - 17] = getInput(i).getSignal();
+        for(int i = 34; i < 66; ++i) {
+            memory[address() + i - 34] = getInput(i).getSignal();
         }
     }
 
@@ -40,9 +50,11 @@ public class Memory extends Node {
 
     @Override
     public void evaluate() {
-        if (getInput(0).getSignal()) {
+        if (getInput(1).getSignal()) {
             memoryWrite();
-        } else {
+        } else
+        if (getInput(1).getSignal()) {
+
             memoryRead();
         }
     }
