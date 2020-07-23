97f363a8a638e03136d97596bf49d34c57a2ac0d
kowsar <kowsar.sh2000@gail.com>
2020-07-23 20:33:02 +0430

    Update Sample.java

--- GraphBasedMinimalistCircuitSimulator-master/src/simulator/Sample.java
+++ GraphBasedMinimalistCircuitSimulator-master/src/simulator/Sample.java
@@ -45,26 +45,26 @@ public class Sample {
 //        Simulator.debugger.addTrackItem(alu);
 
 
-        PC myPC = new PC("myPC", "32X32");
-        NextPcValue pc=new NextPcValue("updatePC","61x32");
+        PC pc = new PC("PC", "32X32");
+        NextPcValue nextPcValue=new NextPcValue("updatePC","61x32");
 
 
-        myPC.addInput(
+        pc.addInput(
 
+                nextPcValue.getOutput(0),nextPcValue.getOutput(1),nextPcValue.getOutput(2),nextPcValue.getOutput(3),nextPcValue.getOutput(4),nextPcValue.getOutput(5),nextPcValue.getOutput(6),nextPcValue.getOutput(7),
+                nextPcValue.getOutput(8),nextPcValue.getOutput(9),nextPcValue.getOutput(10),nextPcValue.getOutput(11),nextPcValue.getOutput(12),nextPcValue.getOutput(13),nextPcValue.getOutput(14),nextPcValue.getOutput(15),
+                nextPcValue.getOutput(16),nextPcValue.getOutput(17),nextPcValue.getOutput(18),nextPcValue.getOutput(19),nextPcValue.getOutput(20),nextPcValue.getOutput(21),nextPcValue.getOutput(22),nextPcValue.getOutput(23),
+                nextPcValue.getOutput(24),nextPcValue.getOutput(25),nextPcValue.getOutput(26),nextPcValue.getOutput(27),nextPcValue.getOutput(28),nextPcValue.getOutput(29),nextPcValue.getOutput(30),nextPcValue.getOutput(31)
+
+        );
+
+        Simulator.debugger.addTrackItem(pc);
+        nextPcValue.addInput(
                 pc.getOutput(0),pc.getOutput(1),pc.getOutput(2),pc.getOutput(3),pc.getOutput(4),pc.getOutput(5),pc.getOutput(6),pc.getOutput(7),
                 pc.getOutput(8),pc.getOutput(9),pc.getOutput(10),pc.getOutput(11),pc.getOutput(12),pc.getOutput(13),pc.getOutput(14),pc.getOutput(15),
                 pc.getOutput(16),pc.getOutput(17),pc.getOutput(18),pc.getOutput(19),pc.getOutput(20),pc.getOutput(21),pc.getOutput(22),pc.getOutput(23),
-                pc.getOutput(24),pc.getOutput(25),pc.getOutput(26),pc.getOutput(27),pc.getOutput(28),pc.getOutput(29),pc.getOutput(30),pc.getOutput(31)
+                pc.getOutput(24),pc.getOutput(25),pc.getOutput(26),pc.getOutput(27),pc.getOutput(28),pc.getOutput(29),pc.getOutput(30),pc.getOutput(31),
 
-                );
-
-        Simulator.debugger.addTrackItem(myPC);
-        pc.addInput(
-                myPC.getOutput(0),myPC.getOutput(1),myPC.getOutput(2),myPC.getOutput(3),myPC.getOutput(4),myPC.getOutput(5),myPC.getOutput(6),myPC.getOutput(7),
-                myPC.getOutput(8),myPC.getOutput(9),myPC.getOutput(10),myPC.getOutput(11),myPC.getOutput(12),myPC.getOutput(13),myPC.getOutput(14),myPC.getOutput(15),
-                myPC.getOutput(16),myPC.getOutput(17),myPC.getOutput(18),myPC.getOutput(19),myPC.getOutput(20),myPC.getOutput(21),myPC.getOutput(22),myPC.getOutput(23),
-                myPC.getOutput(24),myPC.getOutput(25),myPC.getOutput(26),myPC.getOutput(27),myPC.getOutput(28),myPC.getOutput(29),myPC.getOutput(30),myPC.getOutput(31),
-                
                 Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
                 Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
                 Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
@@ -73,8 +73,28 @@ public class Sample {
                 Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic
 
         );
+        Memory I_Memory=new Memory("I_Memory",Simulator.trueLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic);
 
-        Simulator.debugger.addTrackItem(myPC,pc);
+
+
+        memory.evaluate();
+        Simulator.debugger.addTrackItem(pc,nextPcValue,memory);
         Simulator.debugger.setDelay(500);
         Simulator.circuit.startCircuit();
     }
