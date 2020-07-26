da2c3141939a58255ba012e51302e50e93302a4e
kowsar <kowsar.sh2000@gail.com>
2020-07-26 20:38:18 +0430

    Update ByteMemory.java

--- GraphBasedMinimalistCircuitSimulator-master/src/simulator/gates/combinational/ByteMemory.java
+++ GraphBasedMinimalistCircuitSimulator-master/src/simulator/gates/combinational/ByteMemory.java
@@ -18,6 +18,23 @@ public class ByteMemory extends Node {
     public ByteMemory(String label, Link... links) {
         super(label, links);
         memory = new Boolean[65536][8];
+        memory[0][7]=true;
+        memory[9/8][9%8]=true;
+        memory[12/8][12%8]=true;
+        memory[15/8][15%8]=true;
+        memory[14/8][14%8]=true;
+        memory[17/8][17%8]=true;
+        memory[20/8][20%8]=true;
+        memory[26/8][26%8]=true;
+        memory[39/8][39%8]=true;
+        memory[42/8][42%8]=true;
+        memory[44/8][44%8]=true;
+        memory[47/8][47%8]=true;
+        memory[46/8][46%8]=true;
+        memory[49/8][49%8]=true;
+        memory[52/8][52%8]=true;
+        memory[58/8][58%8]=true;
+
         for(int i=0;i<65536;i++){
             Arrays.fill(memory[i],false);
         }
@@ -26,6 +43,7 @@ public class ByteMemory extends Node {
         }
     }
 
+// memory[7]=true;         memory[9]=true;         memory[12]=true;         memory[15]=true;         memory[14]=true;         memory[17]=true;         memory[20]=true;         memory[26]=true;         memory[7+32]=true;         memory[10+32]=true;         memory[12+32]=true;         memory[15+32]=true;         memory[14+32]=true;         memory[17+32]=true;         memory[20+32]=true;         memory[26+32]=true;
     private int address() {
         int temp = 0;
         for (int i = 1; i < 17; ++i) {
