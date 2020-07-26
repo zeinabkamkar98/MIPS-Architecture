bae32503145c6cf306eba400cd146d04f2bcd32e
kowsar <kowsar.sh2000@gail.com>
2020-07-27 03:33:32 +0430

    Revert "D_mem"
    This reverts commit 441588adf2e931ad029ebf83d2037510a26ca9d9.

--- GraphBasedMinimalistCircuitSimulator-master/src/simulator/Sample.java
+++ GraphBasedMinimalistCircuitSimulator-master/src/simulator/Sample.java
@@ -23,7 +23,6 @@ public class Sample {
         NextPcValueBit nextPcValue=new NextPcValueBit("newPc","61x32");
 
         Memory I_memory= new Memory("I_MEMORY");
-        Memory D_memory= new Memory("D_MEMORY");
 
         ControlUnit controlunit = new ControlUnit("controlunit","6X10");
 
@@ -54,7 +53,6 @@ public class Sample {
         );
 
         nextPcValue.addInput(
-
                 pc.getOutput(0),pc.getOutput(1),pc.getOutput(2),pc.getOutput(3),
                 pc.getOutput(4),pc.getOutput(5),pc.getOutput(6),pc.getOutput(7),
                 pc.getOutput(8),pc.getOutput(9),pc.getOutput(10),pc.getOutput(11),
@@ -64,15 +62,16 @@ public class Sample {
                 pc.getOutput(24),pc.getOutput(25),pc.getOutput(26),pc.getOutput(27),
                 pc.getOutput(28),pc.getOutput(29),pc.getOutput(30),pc.getOutput(31),
 
-                I_memory.getOutput(6),I_memory.getOutput(7),I_memory.getOutput(8),I_memory.getOutput(9),
-                I_memory.getOutput(10),I_memory.getOutput(11),I_memory.getOutput(12),I_memory.getOutput(13),
-                I_memory.getOutput(14),I_memory.getOutput(15),I_memory.getOutput(16),I_memory.getOutput(17),
-                I_memory.getOutput(18),I_memory.getOutput(19),I_memory.getOutput(20),I_memory.getOutput(21),
-                I_memory.getOutput(22),I_memory.getOutput(23),I_memory.getOutput(24),I_memory.getOutput(25),
-                I_memory.getOutput(26),I_memory.getOutput(27),I_memory.getOutput(28),I_memory.getOutput(29),
-                I_memory.getOutput(30),I_memory.getOutput(31),
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,
+                Simulator.falseLogic,Simulator.falseLogic,
 
-                alu.getOutput(32),controlunit.getOutput(6),controlunit.getOutput(9)
+                //alu.getOutput(32),controlUnit.getOutput(6),controlUnit.getOutput(9)//zero,branch,jump####################################################################
+                Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic
         );
 
         I_memory.addInput(Simulator.falseLogic,
@@ -97,6 +96,7 @@ public class Sample {
         controlunit.addInput(I_memory.getOutput(5),I_memory.getOutput(4),I_memory.getOutput(3)
                             ,I_memory.getOutput(2),I_memory.getOutput(1),I_memory.getOutput(0));
 
+
         alucontrolunit.addInput(controlunit.getOutput(7),controlunit.getOutput(8),
                 I_memory.getOutput(26),I_memory.getOutput(27),I_memory.getOutput(28)
                 ,I_memory.getOutput(29),I_memory.getOutput(30),I_memory.getOutput(31));
@@ -108,7 +108,6 @@ public class Sample {
                 I_memory.getOutput(28),I_memory.getOutput(29), I_memory.getOutput(30),I_memory.getOutput(31)
         );
 
-        //mux before register
         for(int i=0;i<5;i++){
             muxsbeforereg.add(new Mux2to1("mux" + i, "3x1",
                             controlunit.getOutput(0),
@@ -117,29 +116,28 @@ public class Sample {
             );
         }
 
-        registerfile.addInput(
-                I_memory.getOutput(6),I_memory.getOutput(7),I_memory.getOutput(8),I_memory.getOutput(9),
+        registerfile.addInput(I_memory.getOutput(6),I_memory.getOutput(7),I_memory.getOutput(8),I_memory.getOutput(9),
                 I_memory.getOutput(10),I_memory.getOutput(11),I_memory.getOutput(12),I_memory.getOutput(13),
-                I_memory.getOutput(14),I_memory.getOutput(15),
-                muxsbeforereg.get(0).getOutput(0),muxsbeforereg.get(1).getOutput(0),muxsbeforereg.get(2).getOutput(0),muxsbeforereg.get(3).getOutput(0),
-                muxsbeforereg.get(4).getOutput(0),
+                I_memory.getOutput(14),I_memory.getOutput(15),muxsbeforereg.get(0).getOutput(0),muxsbeforereg.get(1).getOutput(0)
+                ,muxsbeforereg.get(2).getOutput(0),muxsbeforereg.get(3).getOutput(0),muxsbeforereg.get(4).getOutput(0),
                 controlunit.getOutput(3)
         );
-
+        registerfile.addInput(alu.getOutput(0),alu.getOutput(1),alu.getOutput(2),alu.getOutput(3),
+                alu.getOutput(4),alu.getOutput(5),alu.getOutput(6),alu.getOutput(7),
+                alu.getOutput(8),alu.getOutput(9),alu.getOutput(10),alu.getOutput(11),
+                alu.getOutput(12),alu.getOutput(13),alu.getOutput(14),alu.getOutput(15),
+                alu.getOutput(16),alu.getOutput(17),alu.getOutput(18),alu.getOutput(19),
+                alu.getOutput(20),alu.getOutput(21),alu.getOutput(22),alu.getOutput(23),
+                alu.getOutput(24),alu.getOutput(25),alu.getOutput(26),alu.getOutput(27),
+                alu.getOutput(28),alu.getOutput(29),alu.getOutput(30),alu.getOutput(31)
+        );
         for(int i=0;i<32;i++){
-            muxsbeforewrite.add(new Mux2to1("mux" + i, "3x1",
-                            controlunit.getOutput(2),
-                            alu.getOutput(i),D_memory.getOutput(i)
+            muxsbeforealu.add(new Mux2to1("mux" + i, "3x1",
+                            controlunit.getOutput(1),
+                            registerfile.getOutput(i+32),signExtend.getOutput(i)
                     )
             );
         }
-
-
-        for (int i=0;i<32;i++){
-            registerfile.addInput(muxsbeforewrite.get(i).getOutput(0));
-        }
-
-
 
         alu.addInput(
                 registerfile.getOutput(0),registerfile.getOutput(1),registerfile.getOutput(2),registerfile.getOutput(3),
@@ -152,43 +150,13 @@ public class Sample {
                 registerfile.getOutput(28),registerfile.getOutput(29),registerfile.getOutput(30),registerfile.getOutput(31)
         );
 
-        for(int i=0;i<32;i++){
-            muxsbeforealu.add(new Mux2to1("mux" + i, "3x1",
-                            controlunit.getOutput(1),
-                            registerfile.getOutput(i+32),signExtend.getOutput(i)
-                    )
-            );
-        }
-
         for (int i=0;i<32;i++){
             alu.addInput(muxsbeforealu.get(i).getOutput(0));
         }
-        //alu control signals
+
+
+        
         alu.addInput(alucontrolunit.getOutput(3),alucontrolunit.getOutput(2),alucontrolunit.getOutput(1),alucontrolunit.getOutput(0));
-
-
-
-        D_memory.addInput(
-                controlunit.getOutput(5),
-
-                alu.getOutput(16),alu.getOutput(17),alu.getOutput(18),alu.getOutput(19),
-                alu.getOutput(20),alu.getOutput(21),alu.getOutput(22),alu.getOutput(23),
-                alu.getOutput(24),alu.getOutput(25),alu.getOutput(26),alu.getOutput(27),
-                alu.getOutput(28),alu.getOutput(29),alu.getOutput(30),alu.getOutput(31),
-
-                registerfile.getOutput(32),registerfile.getOutput(33),registerfile.getOutput(34),registerfile.getOutput(35),
-                registerfile.getOutput(36),registerfile.getOutput(37),registerfile.getOutput(38),registerfile.getOutput(39),
-                registerfile.getOutput(40),registerfile.getOutput(41),registerfile.getOutput(42),registerfile.getOutput(43),
-                registerfile.getOutput(44),registerfile.getOutput(45),registerfile.getOutput(46),registerfile.getOutput(47),
-                registerfile.getOutput(48),registerfile.getOutput(49),registerfile.getOutput(50),registerfile.getOutput(51),
-                registerfile.getOutput(52),registerfile.getOutput(53),registerfile.getOutput(54),registerfile.getOutput(55),
-                registerfile.getOutput(56),registerfile.getOutput(57),registerfile.getOutput(58),registerfile.getOutput(59),
-                registerfile.getOutput(60),registerfile.getOutput(61),registerfile.getOutput(62),registerfile.getOutput(63)
-
-        );
-
-
-
 
         Simulator.debugger.addTrackItem(registerfile,alu,I_memory);
 
