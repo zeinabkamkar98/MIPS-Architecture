3c40b121bb2ec8591300721544c954a96804a8f3
saina78 <daneshmand.saina@gmail.com>
2020-07-29 10:05:35 +0430

    Revert "Cache Added"
    This reverts commit a64f9d7559e17e68bf10c66bb1c2f73643731740.

--- GraphBasedMinimalistCircuitSimulator-master/src/simulator/wrapper/wrappers/LOD.java
+++ GraphBasedMinimalistCircuitSimulator-master/src/simulator/wrapper/wrappers/LOD.java
@@ -1,8 +1,6 @@
 package simulator.wrapper.wrappers;
 
-import simulator.Sample;
 import simulator.control.Simulator;
-import simulator.gates.combinational.And;
 import simulator.gates.sequential.Clock;
 import simulator.network.Link;
 import simulator.wrapper.Wrapper;
@@ -12,52 +10,33 @@ import simulator.wrapper.Wrapper;
  * input2,514:write data
  * */
 public class LOD extends Wrapper {
-    public SubCache[] LOD;
+    private SubCache[] LOD;
     public LOD(String label, String stream, Link... links) {
         super(label, stream, links);
     }
 
     @Override
     public void initialize() {
-        LOD=new SubCache[128];
-        for (int i = 0; i < 128; i++) {
-            LOD[i]=new SubCache("element"+i,"3x1", Sample.clock.getOutput(0), Simulator.falseLogic, Simulator.falseLogic);
+        LOD=new SubCache[512];
+        for (int i = 0; i < 512; i++) {
+            LOD[i]=new SubCache("element"+i,"3x1", new Clock("S",100).getOutput(0), Simulator.falseLogic, Simulator.falseLogic);
 
         }
-        for (int i = 0; i <128 ; i++) {
-            addOutput(Simulator.falseLogic);
-
-        }
-
-
-
-        for (int i = 0; i < 128; i++) {
-            LOD[i].setInput(1,getInput(1));
-            LOD[i].setInput(2,getInput(i+2));
-
-
-        }
-        for (int i = 0; i < 128; i++) {
-            setOutput(i,new And("a",getInput(0),LOD[i].getOutput(0)).getOutput(0));
-
-        }
-
+        addOutput(Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.falseLogic);
 
     }
     public void read(){
         for (int i = 0; i < 512; i++) {
-//            addOutput(LOD[i].getOutput(0));
+            this.addOutput(LOD[i].getOutput(0));
 
         }
     }  public void write(){
         for (int i = 0; i < 512; i++) {
             LOD[i].setInput(1,getInput(1));
             LOD[i].setInput(2,getInput(i+2));
-            System.out.println(getInput(i+2).getSignal());
 
 
         }
-
     }
 
     public SubCache[] getLOD() {
